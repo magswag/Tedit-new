94de63f4f8f19e7f0cf65cae60665a8d79fd4053
magswag <magnus.o.randby@gmail.com>
2015-07-22 15:34:22 +0200

    -Added launch screen and Intent to start EditActivity.class

--- Tedit-new2/app/src/main/java/magapps/tedit_new/EditActivity.java
+++ Tedit-new2/app/src/main/java/magapps/tedit_new/EditActivity.java
@@ -1,31 +1,24 @@
 package magapps.tedit_new;
 
-import android.content.Intent;
-import android.os.Vibrator;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 
-public class LaunchActivity extends ActionBarActivity {
-    public View start;
+public class EditActivity extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_launch);
-        start = (View) findViewById(R.id.start_button);
-
-        }
-
-
+        overridePendingTransition(R.anim.zoom_in, R.anim.zoom_out);
+        setContentView(R.layout.activity_edit);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_launch, menu);
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
         return true;
     }
 
@@ -42,19 +35,5 @@ public class LaunchActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void start_on_click(View view) {
-        Vibrator vib = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        vib.vibrate(90);
-        Intent startIntent = new Intent(LaunchActivity.this, EditActivity.class);
-        startActivity(startIntent);
-    }
-
-    @Override
-    public void onBackPressed() {
-        this.finish();
-        overridePendingTransition(R.anim.zoom_in, R.anim.zoom_out);
-        super.onBackPressed();
     }
 }
